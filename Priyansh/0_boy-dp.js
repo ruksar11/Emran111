8b609170034fc012c693003f6d9fd69e06523075
ruksar11 <ruksarkhan831847@gmail.com>
2024-11-26 21:04:55 +0530

    Update 0_boy-dp.js

--- Priyansh/0_boy-dp.js
+++ Priyansh/0_boy-dp.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "boydp",
   version: "1.0.0",
   hasPermssion: 0,
-  credits: "PREM BABU",
+  credits: "KHAN SAHAB",
   description: "Boys Dp photos",
   commandCategory: "Random-IMG",
   usages: "Boy dp",
